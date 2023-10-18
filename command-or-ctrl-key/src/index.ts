4c4a00054bc48fe5d776577e59cdc0ef5c0345a4
Abhijith Hegde <60565384+abhijithhegde@users.noreply.github.com>
2023-10-18 23:09:49 +0530

    Update index.ts

--- command-or-ctrl-key/src/index.ts
+++ command-or-ctrl-key/src/index.ts
@@ -2,12 +2,22 @@
  * Get the appropriate keyboard shortcut key based on the user's platform.
  * Returns '⌘' for macOS and 'Ctrl' for other operating systems.
  */
-export const getCommandOrCtrlKey = () => {
-  const userAgent = navigator.userAgent.toLowerCase();
+export const getCommandOrCtrlKey = ({
+	mac = "⌘",
+	others = "Ctrl",
+}: { mac?: string; others?: string } = {}): string => {
+	const userAgent = navigator.userAgent.toLowerCase();
 
-  if (userAgent.includes("mac")) {
-    return "⌘"; // macOS
-  } else {
-    return "Ctrl"; // Other operating systems
-  }
+	if (userAgent.includes("mac")) {
+		return mac; // macOS
+	} else {
+		return others; // Other operating systems
+	}
 };
+//call the function with default values
+const defaultKey = getCommandOrCtrlKey();
+console.log(defaultKey); // '⌘' or 'Ctrl'
+
+//call the function with custom values
+const customKey = getCommandOrCtrlKey({ mac: "cmd", others: "control" });
+console.log(customKey); // 'cmd' or 'control'
